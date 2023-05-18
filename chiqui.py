34b99ddbcc9bedfe3186e87bd5c81a710362c011
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-05-18 14:13:46 -0300

    feat: llaves terminadas

--- chiqui.py
+++ chiqui.py
@@ -53,21 +53,42 @@ def simular_jugadas(a, b, cantidad_jugadas):
 
 lista_equipos = [("River", 78),("Boca", 37),("Independiente", 40),("Racing", 55),("San Lorenzo", 64),("Banfield", 17),("Velez", 47),("Huracán", 21)]
 
-for i in range(0, len(lista_equipos), 2):
-    a= lista_equipos[i]
-    b = lista_equipos [i+1] 
-    puntosA,puntosB = simular_jugadas(a, b, 4)
-    print(f"Inicial {a[0]}:{puntosA} | {b[0]}:{puntosB}")
-    if puntosA == puntosB:
-        dpuntosA, dpuntosB = simular_jugadas(a, b, 2)
-        puntosA = puntosA + dpuntosA
-        puntosB = puntosB + dpuntosB
-        print(f"Tiempo extra {a[0]}:{puntosA} | {b[0]}:{puntosB}")
-    while puntosA == puntosB:
-        #gol de oro
-        dpuntosA, dpuntosB = simular_jugadas(a, b, 1)
-        puntosA = puntosA + dpuntosA
-        puntosB = puntosB + dpuntosB
-        print(f"Gol de oro {a[0]}:{puntosA} | {b[0]}:{puntosB}")
+def correr_lista(lista_elegida):
     
+    lista_ganadores =[]
+    for i in range(0, len(lista_elegida), 2):
+        
+        a= lista_equipos[i]
+        b = lista_equipos [i+1] 
+        #ganador 
+        puntosA,puntosB = simular_jugadas(a, b, 4)
+        print(f"Inicial {a[0]}:{puntosA} | {b[0]}:{puntosB}")
+        if puntosA == puntosB:
+            dpuntosA, dpuntosB = simular_jugadas(a, b, 2)
+            puntosA = puntosA + dpuntosA
+            puntosB = puntosB + dpuntosB
+            print(f"Tiempo extra {a[0]}:{puntosA} | {b[0]}:{puntosB}")
+        while puntosA == puntosB:
+            #gol de oro
+            dpuntosA, dpuntosB = simular_jugadas(a, b, 1)
+            puntosA = puntosA + dpuntosA
+            puntosB = puntosB + dpuntosB
+            print(f"Gol de oro {a[0]}:{puntosA} | {b[0]}:{puntosB}")
+        if puntosA>puntosB: 
+            #ganador = a
+            print(f"El ganador es {a[0]}")
+            lista_ganadores.append(a)
+        elif puntosB>puntosA:
+            #ganador = b 
+            print(f"El ganador es {b [0]}")
+            lista_ganadores.append(b)
+
+    return lista_ganadores
+
+ganadores1 = correr_lista(lista_equipos)
+print(f"los ganadores de la primera ronda son {ganadores1}")
+ganadores2 = correr_lista(ganadores1)
+print(f"Los ganadores de la ronda 2 son {ganadores2}")
+ganadores3= correr_lista(ganadores2)
+print(f"El ganador es {ganadores3}")
 
